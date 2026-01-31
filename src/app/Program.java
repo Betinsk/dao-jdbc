db47f292401e32f4f66504aad5cc680f795ff7b0
Betinsk <matheusf730@gmail.com>
2026-01-31 11:01:13 -0300

    Department

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Department department = new Department(1, "books");
+		System.out.println(department);
+	}
+
+}
