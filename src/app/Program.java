00224836915f22830eeb12b7eeaef92b32ac2f13
Betinsk <matheusf730@gmail.com>
2026-01-31 11:24:47 -0300

    Seller

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,11 @@ public class Program {
 
 		Department department = new Department(1, "books");
 		System.out.println(department);
+		
+		Seller seller = new Seller(21, "Donalt", "doonal@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
+
 	}
 
 }
